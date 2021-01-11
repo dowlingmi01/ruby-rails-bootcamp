7b068b5475d22984f46a1cda329de7944d0d0d5d
Michael <michaeldowling440@gmail.com>
2021-01-11 21:22:52 +0000

    fixed fontawesome in application.js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,20 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
-import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap'
-// import "font-awesome-sprockets"
-// import "font-awesome"
-import "@fortawesome/fontawesome-free/css/all"
-require("stylesheets/application.scss")
-require("trix")
-require("@rails/actiontext")
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap';
+import "@fortawesome/fontawesome-free/css/all";
+require("stylesheets/application.scss");
+require("trix");
+require("@rails/actiontext");
