a3500363350ea11ec2883dfd6acbeec59d48eec4
Michael <michaeldowling440@gmail.com>
2021-01-11 21:31:30 +0000

    minor change

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,31 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
+// 
 
-Rails.start();
-Turbolinks.start();
-ActiveStorage.start();
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
 
-import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap/dist/css/bootstrap';
-import "@fortawesome/fontawesome-free/css/all";
-require("stylesheets/application.scss");
-require("trix");
-require("@rails/actiontext");
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+import 'bootstrap/dist/js/bootstrap'
+import "bootstrap/dist/css/bootstrap";
+
+import "@fortawesome/fontawesome-free/css/all"
+
+require("stylesheets/application.scss")
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
