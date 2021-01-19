e8e5930008117129169752f6ea0ebea6475dea52
Michael <michaeldowling440@gmail.com>
2021-01-19 15:41:12 +0000

    update to trackable db

--- db/migrate/20210114150453_add_trackable_to_devise.rb
+++ db/migrate/20210114150453_add_trackable_to_devise.rb
@@ -1,5 +1,5 @@
 class AddTrackableToDevise < ActiveRecord::Migration[6.1]
-  def up
+  def change
     add_column :users, :sign_in_count, :integer, default: 0, null: false
     add_column :users, :current_sign_in_at, :datetime
     add_column :users, :last_sign_in_at, :datetime
