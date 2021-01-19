1d6c5b37828a408705b1bdf9f7e0d49c8f5a1fb3
Michael <michaeldowling440@gmail.com>
2021-01-19 02:54:00 +0000

    heroku username and password

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net', 
   :port => '587', 
   :authentication => :plain, 
-  :user_name => ENV['SENDGRID_USERNAME'], 
-  :password => ENV['SENDGRID_PASSWORD'], 
+  :user_name => ENV['app197460246@heroku.com'], 
+  :password => ENV['cnvwz3cw8632'], 
   :domain => 'heroku.com', 
   :enable_starttls_auto => true 
 }
