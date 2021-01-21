4d10ef6538c2265b042ad506e5315fdeda62617f
Michael <michaeldowling440@gmail.com>
2021-01-21 23:19:43 +0000

    sendgrid fix

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,10 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net', 
   :port => '587', 
   :authentication => :plain, 
-  :user_name => ENV['michaeldowling440@gmail.com'], 
-  :password => ENV['Tr00p3r&P0lly@r3d0gs'], 
+  :user_name => ENV['SENDGRID_USERNAME'], 
+  :password => ENV['SENDGRID_PASSWORD'], 
+  # :user_name => ENV['michaeldowling440@gmail.com'], 
+  # :password => ENV['Tr00p3r&P0lly@r3d0gs'], 
   :domain => 'heroku.com', 
   :enable_starttls_auto => true 
 }
