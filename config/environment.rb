3105d036f54293b840dc3dbcfd674c5f6119f597
Michael <michaeldowling440@gmail.com>
2021-01-22 00:14:17 +0000

    email configuration changes

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,13 @@ require_relative "application"
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net', 
+  :address => 'email-smtp.eu-central-1.amazonaws.com', 
   :port => '587', 
   :authentication => :plain, 
-  :user_name => ENV['SENDGRID_USERNAME'], 
-  :password => ENV['SENDGRID_PASSWORD'], 
+  :user_name => ENV['SMTP_CREDENTIALS_USER_NAME'], 
+  :password => ENV['SMTP_CREDENTIALS_PASSWORD'], 
   # :user_name => ENV['michaeldowling440@gmail.com'], 
   # :password => ENV['Tr00p3r&P0lly@r3d0gs'], 
-  :domain => 'heroku.com', 
+  :domain => 'unlikely-leader-lms.herokuapp.com', 
   :enable_starttls_auto => true 
 }
