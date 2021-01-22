ebe5728254d03111d9b43106cb371ca9bfc3a860
Michael <michaeldowling440@gmail.com>
2021-01-22 02:00:31 +0000

    debug heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: "unlikely-leader-lms.herokuapp.com", protocol: "https"}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.log_level = :debug
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
